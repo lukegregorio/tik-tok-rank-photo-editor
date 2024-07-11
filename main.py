6fa043011e0551de5c8bb400e70fe6b61bbcbda1
lukegregorio <lukegregorio99@gmail.com>
2024-07-11 11:30:26 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+
+
+def main(width=1000, height=1000):
+    # open image files in images folder and save them in a list
+    images = []
+
+    for filename in os.listdir('images'):
+        img = Image.open(f'images/{filename}')
+        images.append(img)
+
+    # amend all the images to have the same size 1000x1000
+    for i in range(len(images)):
+        images[i] = images[i].resize((width, height))
+
+    # convert all the images to png format
+    for i in range(len(images)):
+        images[i].save(f'resized_images/{i}.png')
